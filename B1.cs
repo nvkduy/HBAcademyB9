421e923e17dd24624f4ccf3cd8dc0d3f8305cb1f
HBGIANGVIEN2 <HBGIANGVIEN2@HB-ACADEMY-1>
2024-07-07 19:10:51 +0700

    Add project files.

---
+++ B1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBAcademyB9
+{
+    internal class B1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace HBAcademyB9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+        }
+    }
+}
